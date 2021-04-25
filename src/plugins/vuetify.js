9122d2b78b566358a9999fd5521753d502d8e14e
burshtei <menachem.burshtein@intel.com>
2021-04-25 14:23:20 +0300

    add vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import he from '../locales/he'
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  rtl: true,
+  lang: {
+    locales: { he },
+    current: 'he'
+  }
+})
