1fec325c3e3b2d96a29ffd86e1b8a6d64cf1c915
burshtei <menachem.burshtein@intel.com>
2021-04-25 15:21:55 +0300

    router

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <NavBar></NavBar>
     <v-main>
-    <router-view />
+      <router-view />
     </v-main>
   </v-app>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 import Actions from "@/components/Actions";
 import Inventory from "@/components/Inventory";
 import ExportImport from "@/components/ExportImport";
@@ -11,7 +10,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Inventory,
   },
   {
     path: "/actions",
@@ -27,15 +26,6 @@ const routes = [
     path: "/exportImport",
     name: "ExportImport",
     component: ExportImport,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
 ];
 
