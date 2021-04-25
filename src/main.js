40fd556396631051e898f8a9764f1fc774178110
burshtei <menachem.burshtein@intel.com>
2021-04-25 22:10:42 +0300

    add icons

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import i18n from "@/plugins/i18n"; // path to vuetify export
-
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.config.productionTip = false;
 
