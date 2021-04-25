64e849953f1aa65a02f00ef1ab974434fd944c7c
burshtei <menachem.burshtein@intel.com>
2021-04-25 16:46:58 +0300

    mobile

--- src/components/Actions.vue
+++ src/components/Actions.vue
@@ -1,81 +1,84 @@
 <template>
   <v-card>
-  <v-data-table
-    dense
-    :headers="headers"
-    :items="actions"
-    :search="search"
-    class="elevation-1"
-  >
-    <template v-slot:item.date="{ item }">
-      {{ moment(item.date).format("DD/MM/YYYY") }}
-    </template>
-    <template v-slot:item.amount="{ item }">
-      <v-chip
-        :color="getColor(item.amount)">
-        {{signedNumber(item.amount)}}
-      </v-chip>
-    </template>
-    <template v-slot:item.target="{ item }">
-      {{ targetDisplay(item.target)}}
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize"> Reset</v-btn>
-    </template>
-  </v-data-table>
+    <v-data-table
+      dense
+      :headers="headers"
+      :items="actions"
+      :search="search"
+      class="elevation-1"
+      mobile-breakpoint="0"
+    >
+      <template v-slot:item.date="{ item }">
+        {{ moment(item.date).format("DD/MM/YYYY") }}
+      </template>
+      <template v-slot:item.amount="{ item }">
+        <v-chip :color="getColor(item.amount)">
+          {{ signedNumber(item.amount) }}
+        </v-chip>
+      </template>
+      <template v-slot:item.target="{ item }">
+        {{ targetDisplay(item.target) }}
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="initialize"> Reset</v-btn>
+      </template>
+    </v-data-table>
   </v-card>
 </template>
 <script>
-import axios from 'axios'
-import * as moment from 'moment'
+import axios from "axios";
+import * as moment from "moment";
 
 export default {
   data: () => ({
-    backendUrl: 'https://kehat-backend.herokuapp.com',
+    backendUrl: "https://kehat-backend.herokuapp.com",
     dialog: false,
     dialogDelete: false,
     headers: [
-      { text: 'תאריך', value: 'date' },
-      { text: 'משתמש', value: 'user' },
-      { text: 'ברקוד', value: 'book.barCode' },
-      { text: 'שם', value: 'book.name' },
-      { text: 'כמות', value: 'amount' },
-      { text: 'מטרה', value: 'target' }
+      { text: "תאריך", value: "date" },
+      { text: "משתמש", value: "user" },
+      { text: "ברקוד", value: "book.barCode" },
+      { text: "שם", value: "book.name" },
+      { text: "כמות", value: "amount" },
+      { text: "מטרה", value: "target" },
     ],
-    actions: []
+    actions: [],
   }),
 
-  mounted () {
+  mounted() {
     axios
-      .get(this.backendUrl + '/actions')
+      .get(this.backendUrl + "/actions")
       .then((response) => (this.actions = response.data))
-      .catch((reason) => console.log(reason))
+      .catch((reason) => console.log(reason));
   },
   methods: {
-    initialize () {
-      this.actions = []
+    initialize() {
+      this.actions = [];
     },
     moment: function (date) {
-      return moment(date)
+      return moment(date);
     },
     signedNumber: function (amount) {
-      return amount
+      if (amount < 0) {
+        return Math.abs(amount).toString() + "-";
+      }
+      return amount.toString();
     },
 
     targetDisplay: function (target) {
-      return this.$t(target)
+      return this.$t(target);
     },
-    close () {
-      this.dialog = false
+    close() {
+      this.dialog = false;
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
     },
-    getColor (amount) {
-      if (amount > 0) return 'green'
-      else return 'red'
-    }
-  }
-}
+    getColor(amount) {
+      if (amount > 0) return "green";
+      else return "red";
+    },
+  },
+};
 </script>
