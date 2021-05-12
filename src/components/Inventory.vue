ae0b8dea35439f22045b7cc6e5ba88db9d0b538f
Menachem Burshtein <menachem.burshtein@intel.com>
2021-05-12 16:33:53 +0300

    redirect to login

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -1,108 +1,121 @@
 <template>
-  <v-data-table
-    dense
-    :headers="headers"
-    :items="items"
-    sort-by="calories"
-    class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>{{ $t("items") }}</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="800px">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn color="primary" dark v-bind="attrs" v-on="on">
-              פריט חדש
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container fluid>
-                <v-row>
-                  <v-col cols="3">
-                    <v-text-field
-                      v-model="editedItem.barCode"
-                      label="בר קוד"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="4">
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="שם"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="2">
-                    <v-text-field
-                      v-model="editedItem.amount"
-                      label="כמות"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="3">
-                    <v-text-field
-                      v-model="editedItem.packageSize"
-                      label="פריטים בחבילה"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close"> Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="save"> Save</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="headline"
-              >Are you sure you want to delete this item?
-            </v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete"
-                >Cancel
+  <v-card>
+    <v-data-table
+      dense
+      :headers="headers"
+      :items="items"
+      sort-by="barCode"
+      class="elevation-1"
+      :search="search"
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>{{ $t("items") }}</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="800px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                {{ $t("new_item") }}
               </v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                >OK
-              </v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-    <template v-slot:item.actions="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil</v-icon>
-      <v-icon small @click="deleteItem(item)"> mdi-delete</v-icon>
-    </template>
-    <template v-slot:item.amount="{ item }">
-      {{ displayByPackage(item) }}
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize"> Reset</v-btn>
-    </template>
-  </v-data-table>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container fluid>
+                  <v-row>
+                    <v-col cols="3">
+                      <v-text-field
+                        v-model="editedItem.barCode"
+                        label="בר קוד"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="4">
+                      <v-text-field
+                        v-model="editedItem.name"
+                        label="שם"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="2">
+                      <v-text-field
+                        v-model="editedItem.amount"
+                        label="כמות"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="3">
+                      <v-text-field
+                        v-model="editedItem.packageSize"
+                        label="פריטים בחבילה"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close"> Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="save"> Save</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="headline"
+                >Are you sure you want to delete this item?
+              </v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete"
+                  >Cancel
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                  >OK
+                </v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil</v-icon>
+        <v-icon small @click="deleteItem(item)"> mdi-delete</v-icon>
+      </template>
+      <template v-slot:item.amount="{ item }">
+        {{ displayByPackage(item) }}
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="initialize"> Reset</v-btn>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 <script>
 import axios from "axios";
+import router from "@/router";
 
 export default {
   data: () => ({
+    search: "",
     backendUrl: process.env.VUE_APP_BACKEND_API,
     dialog: false,
     dialogDelete: false,
     headers: [
       { text: "בר קוד", value: "barCode" },
       { text: "שם / תיאור", value: "name" },
-      { text: "כמות", value: "amount" },
+      { text: "כמות", value: "amount", filterable: false },
       { text: "פעולות", value: "actions", sortable: false },
     ],
     items: [],
@@ -137,7 +150,12 @@ export default {
     axios
       .get(this.backendUrl + "/books")
       .then((response) => (this.items = response.data))
-      .catch((reason) => console.log(reason));
+      .catch((reason) => {
+        console.log(reason);
+        if (reason.response.status === 401) {
+          router.push("/login");
+        }
+      });
   },
   methods: {
     initialize() {
